7cde94d9ca247f9bac32e554b96c9ef8746d27af
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-26 14:59:50 +0800

    refactor to render page instead of the data of user

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+require('dotenv').config()
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const routes = require('./routes/index');
 const session = require('express-session')
 const flash = require('express-flash')
 const passport = require('passport')
+const config = require('./config/index')
+const path = require('path');
+
+
+var userProfile;
 
 const {
     PORT
@@ -14,6 +20,10 @@ const {
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret: 'this is a secret',
@@ -25,24 +35,27 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser( (user, cb) => { cb(null, user); } )
-passport.deserializeUser( (obj, cb) => { cb(null, obj); } )
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash())
 
 app.set('view-engine', 'ejs');
 
 
 
-app.use(flash())
+passport.serializeUser( (user, cb) => { cb(null, user); } )
+passport.deserializeUser( (obj, cb) => { cb(null, obj); } )
 
-app.use('/', routes);
+passport.use(new GoogleStrategy({
+        clientID: config.googleAuth.clientID, 
+        clientSecret: config.googleAuth.clientSecret, 
+        callbackURL: config.googleAuth.callbackURL, 
+    }, (accessToken, refreshToken, profile, done) => { 
+        return done(null, profile);
+    }
+));
 
 
-app.listen(PORT, () => { console.log(`running at port ${PORT}`) });
 
+app.use(routes);
+app.listen(PORT, () => { console.log(`running at port ${PORT}, the callback is ${config.googleAuth.callbackURL}`) });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config/index')
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
-var userProfile;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/login.ejs');
 });
 
-router.get('/success', (req, res) => res.send(userProfile));
-router.get('/error', (req, res) => res.send('error logging in'));
 
-passport.use(new GoogleStrategy({
-        clientID: config.googleAuth.clientID, 
-        clientSecret: config.googleAuth.clientSecret, 
-        callbackURL: config.googleAuth.callbackURL, 
-    }, (accessToken, refreshToken, profile, done) => { 
-        userProfile = profile;
-        return done(null, userProfile);
-    }
-))
-
-router.get('/auth/google', passport.authenticate('google', { 
+router.get('/auth/google', 
+    passport.authenticate('google', { 
     scope: ['profile', 'email'] 
 }))
 
-router.get('/auth/google/callback', passport.authenticate('google', { 
-    failureRedirect: '/error'
+router.get('/auth/google/callback', 
+    passport.authenticate('google', { 
+        successRedirect: '/success',
+        failureRedirect: '/error'
 }), (req, res) => { res.redirect('/success') })
 
+
+router.get('/success', (req, res) => { 
+    res.render('pages/success.ejs', {user: req.user})
+});
+ 
+router.get('/error', (req, res) => res.send('error logging in'));
 
 module.exports = router;
